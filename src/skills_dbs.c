7199941bab32acda770db9455234ab411c6de655
Zerhyn <darthbutts@gmail.com>
2018-08-15 18:42:32 -0700

    Fixed bugged meditate outputs

--- src/skills_dbs.c
+++ src/skills_dbs.c
@@ -5308,7 +5308,7 @@ do_meditate(CHAR_DATA * ch, char *argument)
 		return;
 	}
 	
-	right = (float) 100;
+	right = (float) ch->pcdata->learned[gsn_meditate] / 100;
 
 	if (ch->fighting) {
 		send_to_char("&wYou can't concentrate enough for that.\n\r",
@@ -5351,24 +5351,25 @@ do_meditate(CHAR_DATA * ch, char *argument)
 					("&wYou meditate peacefully, collecting energy from the cosmos\n\r",
 					ch);
 				learn_from_success(ch, gsn_meditate);
-			if (!IS_NPC(ch)) {
-				if (is_kaio(ch) || is_namek(ch)) {
-					stat_train(ch, "int", 90);
-					statComb = ((get_curr_str(ch) + get_curr_dex(ch) + get_curr_int(ch) + get_curr_con(ch)) - 39);
-					increase = number_range(1,3);
-					xp_gain = (long double)increase / 150 * statComb;
-					gain_exp(ch, xp_gain);
-					ch->mana += (float) right / 50 * ch->max_mana;
-				}
-				else {
-					stat_train(ch, "int", 60);
-					statComb = ((get_curr_str(ch) + get_curr_dex(ch) + get_curr_int(ch) + get_curr_con(ch)) - 39);
-					increase = number_range(1,3);
-					xp_gain = (long double)increase / 200 * statComb;
-					gain_exp(ch, xp_gain);
-					ch->mana += (float) right / 50 * ch->max_mana;
-					if (ch->mana > ch->max_mana) {
-						ch->mana = ch->max_mana;
+				if (!IS_NPC(ch)) {
+					if (is_kaio(ch) || is_namek(ch)) {
+						stat_train(ch, "int", 90);
+						statComb = ((get_curr_str(ch) + get_curr_dex(ch) + get_curr_int(ch) + get_curr_con(ch)) - 39);
+						increase = number_range(1,3);
+						xp_gain = (long double)increase / 150 * statComb;
+						gain_exp(ch, xp_gain);
+						ch->mana += (float) right / 50 * ch->max_mana;
+					}
+					else {
+						stat_train(ch, "int", 60);
+						statComb = ((get_curr_str(ch) + get_curr_dex(ch) + get_curr_int(ch) + get_curr_con(ch)) - 39);
+						increase = number_range(1,3);
+						xp_gain = (long double)increase / 200 * statComb;
+						gain_exp(ch, xp_gain);
+						ch->mana += (float) right / 50 * ch->max_mana;
+						if (ch->mana > ch->max_mana) {
+							ch->mana = ch->max_mana;
+						}
 					}
 				}
 			}
@@ -5400,24 +5401,24 @@ do_meditate(CHAR_DATA * ch, char *argument)
 				learn_from_success(ch, gsn_meditate);
 				if (!IS_NPC(ch)) {
 					if (is_kaio(ch) || is_namek(ch)) {
-					stat_train(ch, "int", 45);
-					statComb = ((get_curr_str(ch) + get_curr_dex(ch) + get_curr_int(ch) + get_curr_con(ch)) - 39);
-					increase = number_range(1,3);
-					xp_gain = (long double)increase / 150 * statComb;
-					gain_exp(ch, xp_gain);
-					ch->mana += (float) right / 50 * ch->max_mana;
-				}
-				else {
-					stat_train(ch, "int", 30);
-					statComb = ((get_curr_str(ch) + get_curr_dex(ch) + get_curr_int(ch) + get_curr_con(ch)) - 39);
-					increase = number_range(1,3);
-					xp_gain = (long double)increase / 200 * statComb;
-					gain_exp(ch, xp_gain);
-					ch->mana += (float) right / 50 * ch->max_mana;
-					if (ch->mana > ch->max_mana) {
-						ch->mana = ch->max_mana;
+						stat_train(ch, "int", 45);
+						statComb = ((get_curr_str(ch) + get_curr_dex(ch) + get_curr_int(ch) + get_curr_con(ch)) - 39);
+						increase = number_range(1,3);
+						xp_gain = (long double)increase / 150 * statComb;
+						gain_exp(ch, xp_gain);
+						ch->mana += (float) right / 50 * ch->max_mana;
 					}
-				}
+					else {
+						stat_train(ch, "int", 30);
+						statComb = ((get_curr_str(ch) + get_curr_dex(ch) + get_curr_int(ch) + get_curr_con(ch)) - 39);
+						increase = number_range(1,3);
+						xp_gain = (long double)increase / 200 * statComb;
+						gain_exp(ch, xp_gain);
+						ch->mana += (float) right / 50 * ch->max_mana;
+						if (ch->mana > ch->max_mana) {
+							ch->mana = ch->max_mana;
+						}
+					}
 				}
 			}
 			else {
