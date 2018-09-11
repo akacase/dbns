726ea7e088d9e73d6ff0775391c093940af6c4e2
Zerhyn <darthbutts@gmail.com>
2018-09-11 15:59:20 -0700

    HHNNGGHH

--- src/act_info.c
+++ src/act_info.c
@@ -4227,20 +4227,6 @@ void do_exercise(CHAR_DATA *ch, char *argument)
 			return;
 		}
 	}
-	else {
-		send_to_char("Exercise how? Available exercises are 'exercise pushup', 'shadowbox', 'endure'.\n\r", ch);
-		send_to_char("Note that 'endure' will have no effect at your body's natural weight.\n\r", ch);
-		if (ch->exintensity = 0) {
-			send_to_char("You're currently prepared for a steady workout.\n\r", ch);
-			send_to_char("To change this intensity, enter 'exercise steadily' or 'intensely'.\n\r", ch);
-			return;
-		}
-		if (ch->exintensity = 1) {
-			send_to_char("You're currently prepared for an intense workout.\n\r", ch);
-			send_to_char("To change this intensity, enter 'exercise steadily' or 'intensely'.\n\r", ch);
-			return;
-		}
-	}
 	return;
 }
 
