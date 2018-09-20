b0bb9b0372323404326072a5d7bb4f0fa281ca71
Zerhyn <darthbutts@gmail.com>
2018-09-20 15:41:54 -0700

    Misc

--- src/update.c
+++ src/update.c
@@ -543,7 +543,7 @@ hit_gain(CHAR_DATA * ch)
 	if (xIS_SET((ch)->affected_by, AFF_INTEXPUSHUPS) || xIS_SET((ch)->affected_by, AFF_EXPUSHUPS)
 	|| xIS_SET((ch)->affected_by, AFF_EXENDURING) || xIS_SET((ch)->affected_by, AFF_INTEXENDURING)
 	|| xIS_SET((ch)->affected_by, AFF_INTEXSHADOWBOXING) || xIS_SET((ch)->affected_by, AFF_EXSHADOWBOXING)
-	|| xIS_SET((ch)->affected_by, AFF_POWERUPTRAIN) {
+	|| xIS_SET((ch)->affected_by, AFF_POWERUPTRAIN)) {
 		gain = 0;
 	}
 
