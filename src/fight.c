61f1a5633873013ca6a41933443e73f2dbb1f110
Zerhyn <darthbutts@gmail.com>
2018-09-20 19:36:18 -0700

    Gain powerlevel on stat gain

--- src/fight.c
+++ src/fight.c
@@ -5328,6 +5328,8 @@ stat_train(CHAR_DATA * ch, char *stat, int *modifier)
 	    *tAbility = 0;
 	    *pAbility += 1;
 	    *permTstat += 1;
+		ch->exp += 1;
+		ch->pl += 1;
 	    if( stat == "str") {
 	        send_to_char( "&CYou feel your strength improving!&D\n\r", ch);
 	    }
@@ -5392,6 +5394,8 @@ exercise_train(CHAR_DATA * ch, char *stat, int *modifier)
 	    *tAbility = 0;
 	    *pAbility += 1;
 	    *permTstat += 1;
+		ch->exp += 1;
+		ch->pl += 1;
 	    if( stat == "str") {
 	        send_to_char( "&CYou feel your strength improving!&D\n\r", ch);
 	    }
