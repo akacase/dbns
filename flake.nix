155bdb19b2773ebc8834365dc933120c41d372a8
case <case@capsulecorp.org>
2023-02-10 16:16:17 -0500

    testin

--- flake.nix
+++ flake.nix
@@ -1,14 +1,83 @@
 {
+  description = "dbns";
+
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.05";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-22.05;
+    utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
-      in {
-        devShell = pkgs.mkShell { buildInputs = [ pkgs.gnumake pkgs.clang pkgs.ninja pkgs.nixfmt pkgs.zlib ]; };
-      });
-}
 
+  outputs = { self, nixpkgs, dns-compliance-testing-src, utils }: utils.lib.eachDefaultSystem
+    (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ self.overlay ];
+        };
+        gcc_musl = with nixpkgs; pkgs.wrapCCWith {
+        cc = pkgs.gcc.cc;
+          bintools = pkgs.wrapBintoolsWith {
+            bintools = pkgs.binutils-unwrapped;
+            libc = pkgs.musl;
+          };
+        };
+        dbns = with pkgs; (overrideCC stdenv gcc_musl).mkDerivation {
+          name = "dbns";
+          src = ./src;
+          nativeBuildInputs = [ clang zlib ninja ];
+          buildInputs = [ openssl.dev ];
+
+          buildPhase = ''
+            ninja
+          '';
+
+          installPhase = ''
+            mkdir -p $out/bin
+            cp ./src/dbnsd $out/bin/dbns
+          '';
+
+          meta = with lib; {
+            description = "DNS protocol compliance of the servers they are delegating zones to.";
+            homepage = https://gitlab.isc.org/isc-projects/DNS-Compliance-Testing;
+            license = licenses.mpl20;
+            maintainers = with maintainers; [ case ];
+          };
+        };
+      in
+      rec
+      {
+        packages.${system} = dbns;
+        defaultPackage = dbns;
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            clang
+            zlib
+            ninja
+          ];
+        };
+      }) // {
+    overlay = final: prev: {
+      dbns = with final; (stdenv.mkDerivation {
+        name = "dbns";
+        src = ./src
+        nativeBuildInputs = [ clang zlib ninja ];
+
+        buildPhase = ''
+          ninja
+        '';
+
+        installPhase = ''
+          mkdir -p $out/bin
+          cp ./src/dbnsd $out/bin/dbnsd
+        '';
+
+        meta = with lib; {
+          description = "Dragonball MUD";
+          homepage = https://capsulecorp.org;
+          license = licenses.agpl;
+          maintainers = with maintainers; [ case ];
+        };
+      });
+    };
+  };
+}
