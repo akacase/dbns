6f50f20c2f09c3f29470bbe7bcb7dd3242967caf
case <case@capsulecorp.org>
2023-02-10 10:44:57 -0500

    nix

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = pkgs.mkShell { buildInputs = [ pkgs.gnumake pkgs.clang pkgs.ninja pkgs.nixfmt ]; };
+      });
+}
+
